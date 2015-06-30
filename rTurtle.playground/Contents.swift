a8e9062c8c4d57763b8c716ffa18aa195c746aaa
Yuan Yuchuan <yyc478@gmail.com>
2015-06-30 13:32:58 +0800

    Added buggy robot turtle playground

---
+++ rTurtle.playground/Contents.swift
@@ -0,0 +1,38 @@
+//: Playground - noun: a place where people can play
+import SpriteKit
+import XCPlayground
+import Cocoa
+
+
+var str = "Hello, playground"
+println(str);
+
+var turtle: RTurtle = RTurtle();
+
+turtle.start()
+
+turtle.moveForward()
+turtle.moveForward()
+turtle.moveForward()
+turtle.moveForward()
+turtle.moveForward()
+turtle.moveForward()
+turtle.moveForward()
+
+turtle.turnRight()
+
+turtle.moveForward()
+turtle.moveForward()
+turtle.moveForward()
+turtle.moveForward()
+turtle.moveForward()
+turtle.moveForward()
+turtle.moveForward()
+turtle.moveForward()
+
+turtle.turnLeft()
+turtle.moveForward()
+turtle.moveForward()
+
+turtle.move()
+turtle.getScore()

3e00caa637b5a75baab020affc4c60193ad66004
Yin Jie Soon <yingjie@pteromacdyl.local>
2015-06-30 09:03:31 +0800

    Added worksheet 1

---
+++ Worksheet1.playground/Contents.swift
@@ -0,0 +1,16 @@
+/*:
+# Intro to Programming in Swift Worksheet 1
+Welcome to Swift! In this worksheet, you'll learn about variables & constants, the console, and doing basic math in Swift.
+*/
+
+/*:
+## Exercise 1
+For this exercise: 
+1. Change the value of **name** to your first name (e.g. "Steven", "Hongyi", "Akmal"), and the value of **lastName** to your surname or family name (e.g. "Chan", "bin Abdul Rahman").
+2. Add a space between your first name and last name.
+3. Create a new variable called **salutation**, and give it the value for yourself (e.g. "Miss", "Mister"). Include it in **fullName**.
+*/
+
+var name = "Yinjie"
+var lastName = "Soon"
+var fullName = name + lastName

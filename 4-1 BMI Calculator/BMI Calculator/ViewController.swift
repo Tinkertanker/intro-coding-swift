de2f7419bff10ea5033edd787bc0c11097dd2503
YJ Soon <yjsoon@tinkertanker.com>
2015-07-30 10:01:39 +0800

    Added BMI calculator skeleton

---
+++ 4-1 BMI Calculator/BMI Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by YJ Soon on 30/7/15.
+//  Copyright (c) 2015 Tinkercademy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

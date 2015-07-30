fd0adb0aec6f5671a3bcf6c73d2ee53384c5ca32
YJ Soon <yjsoon@tinkertanker.com>
2015-07-30 11:14:44 +0800

    Exercise 4.3 done, hiding resultsLabel

--- 4-1 BMI Calculator/BMI Calculator/ViewController.swift
+++ 4-1 BMI Calculator/BMI Calculator/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        resultsField.hidden = true
     }
 
     override func didReceiveMemoryWarning() {
